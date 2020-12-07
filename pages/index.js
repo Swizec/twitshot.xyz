6bb718a3fba0ab448b5766a30fca9d8d53c67af7
Swizec Teller <swizec@swizec.com>
2020-12-07 07:49:55 -0800

    Layout form for tweets

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,20 @@
-import { Heading } from "theme-ui";
+import * as React from "react";
+import { Box, Button, Heading, Input } from "theme-ui";
 import { Layout } from "../components/Layout";
-import styles from "../styles/Home.module.css";
+
+function ScreenshotTaker() {
+    const [tweetUrl, setTweetUrl] = React.useState("");
+    return (
+        <Box as="form" sx={{ pt: [3, 5, 7], textAlign: "center" }}>
+            <Input
+                text={tweetUrl}
+                onChange={(event) => setTweetUrl(event.target.value)}
+                placeholder="Paste tweet URL"
+            />
+            <Button sx={{ mt: 2, cursor: "pointer" }}>Screenshot!</Button>
+        </Box>
+    );
+}
 
 export default function Home() {
     return (
@@ -11,6 +25,8 @@ export default function Home() {
             <Heading sx={{ fontSize: [1, 4, 5], textAlign: "center" }}>
                 screenshots of your tweets
             </Heading>
+
+            <ScreenshotTaker />
         </Layout>
     );
 }
